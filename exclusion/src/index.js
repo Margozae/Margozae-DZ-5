fbe7601eb82a1188813cc10710a7f4a3867de335
Margozae <59208589+Margozae@users.noreply.github.com>
2020-02-23 00:24:47 +0200

    Add files via upload

---
+++ exclusion/src/index.js
@@ -0,0 +1,14 @@
+function foo(str, exclusion) {
+    for(let j = 0; j <= exclusion.length; j++) {
+        let newStr = '';
+        for(let i = 0; i <= str.length - 1; i++) { // eslint-disable-line 
+            if (str[i] !== exclusion[j]) {
+                newStr += str[i];
+            }
+        }
+        str = newStr; // eslint-disable-line 
+    }
+    return str;
+}
+
+foo('Hello', ['o', 'l']);
